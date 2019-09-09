adc5a8cb1edfe1a553c844ce5b9bab7acdf053ec
Ryoichi Izumita <r.izumita@caph.jp>
2019-09-09 21:42:18 +0900

    State binding

--- Sources/Seda/Store.swift
+++ Sources/Seda/Store.swift
@@ -6,8 +6,9 @@
 //
 
 import Foundation
+import SwiftUI
 #if canImport(Combine)
-    import Combine
+import Combine
 #endif
 
 @available(OSX 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
@@ -47,19 +48,29 @@ public class Store<S>: ObservableObject where S: StateType {
     public func selected<SubState: StateType>(_ keyPath: KeyPath<S, SubState>) -> Store<SubState> {
         let result = Store<SubState>(reducer: { _, _ in fatalError() }, state: state[keyPath: keyPath], queue: queue)
         result.parent = AnyStore(self)
-
+        
         $state.map(keyPath).receive(on: queue).sink { state in
             result.state = state
         }.store(in: &cancellables)
-
+        
         return result
+    }
+    
+    public func binding<Value>(_ actionable: @escaping (Value) -> ActionType,
+                               _ keyPath: KeyPath<S, Value>) -> Binding<Value> {
+        Binding(get: { [weak self] in
+            guard let this = self else { fatalError() }
+            return this.state[keyPath: keyPath]
+            }, set: { [weak self] value in
+                self?.dispatch(actionable(value))
+        })
     }
 }
 
 @available(OSX 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
 struct AnyStore {
     let dispatch: (ActionType) -> ()
-
+    
     init<S: StateType>(_ store: Store<S>) {
         self.dispatch = store.dispatch
     }
